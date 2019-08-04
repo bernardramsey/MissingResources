1ffcdc679b53e1070104fc9c97d3661cd664d2c1
g.berezhko <g.berezhko@corp.vk.com>
2019-08-04 15:29:34 +0300

    Implement res generating for main source set

---
+++ app/src/main/java/com/gerralizza/missingresources/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.gerralizza.missingresources
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Toast.makeText(this, R.string.russian_string, Toast.LENGTH_SHORT).show()
+    }
+}
